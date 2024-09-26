7a46a97c9fc2d0cf3d1ece43ae594feda2f9425c
virendra-kashyap <virendrakashyap244@gmail.com>
2024-09-26 13:16:37 +0530

    intergrate spring ai using ollama

---
+++ spring-ai-with-ollama/src/main/java/com/artificial/intelligence/SpringAiWithOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.artificial.intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiWithOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiWithOllamaApplication.class, args);
+	}
+
+}
---
+++ spring-ai-with-ollama/src/test/java/com/artificial/intelligence/SpringAiWithOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artificial.intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiWithOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
