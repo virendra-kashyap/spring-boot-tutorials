4bae8c942a74ba5eca4f16199cdab174022402b7
virendra-kashyap <virendrakashyap244@gmail.com>
2025-01-15 17:47:26 +0530

    implement flyway db migration.

---
+++ spring-with-flywaydb/src/main/java/com/virendra/SpringWithFlywaydbApplication.java
@@ -0,0 +1,13 @@
+package com.virendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithFlywaydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithFlywaydbApplication.class, args);
+	}
+
+}
---
+++ spring-with-flywaydb/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users(
+    user_id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
