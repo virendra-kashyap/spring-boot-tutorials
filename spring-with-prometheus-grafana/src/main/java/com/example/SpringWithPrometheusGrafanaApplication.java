2b5eb24997a4d4db2002ee11a6e2fb654216ca82
virendra-kashyap <virendrakashyap244@gmail.com>
2025-02-06 17:21:34 +0530

    implement prometheus and grafana with spring boot.

---
+++ spring-with-prometheus-grafana/src/main/java/com/example/SpringWithPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithPrometheusGrafanaApplication.class, args);
+	}
+
+}
