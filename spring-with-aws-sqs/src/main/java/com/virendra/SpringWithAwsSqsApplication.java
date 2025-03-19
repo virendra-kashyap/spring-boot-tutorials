3990051fb8cadfcd0210294f9659d93018e8cd30
virendra-kashyap <virendrakashyap244@gmail.com>
2025-03-19 14:39:18 +0530

    Implement Jasypt for encrypting sensitive credentials in application properties

--- spring-with-aws-sqs/src/main/java/com/virendra/SpringWithAwsSqsApplication.java
+++ spring-with-aws-sqs/src/main/java/com/virendra/SpringWithAwsSqsApplication.java
@@ -1,11 +1,13 @@
 package com.virendra;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEncryptableProperties
 public class SpringWithAwsSqsApplication {
 
 	public static void main(String[] args) {
