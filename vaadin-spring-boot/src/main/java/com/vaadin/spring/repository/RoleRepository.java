6b6b241bde2a2c1ca4287aab20c3c71c93b33fcd
Virendra kashyap <66119893+virendra-kashyap@users.noreply.github.com>
2025-05-02 15:07:52 +0530

    Delete vaadin-spring-boot directory

--- vaadin-spring-boot/src/main/java/com/vaadin/spring/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vaadin.spring.dto.RoleDTO;
-
-public interface RoleRepository extends JpaRepository<RoleDTO, Long> {
-
-}
