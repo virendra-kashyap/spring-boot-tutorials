b159ed69227bd623931cb24bde9784495bca8dd0
Virendra kashyap <66119893+virendra-kashyap@users.noreply.github.com>
2025-05-02 15:05:14 +0530

    Delete mock-testing-with-spring-unit directory

--- mock-testing-with-spring-unit/src/main/java/com/virendra/exception/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.virendra.exception;
-
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-public class EntityNotFoundException extends RuntimeException{
-    private final String message;
-}
--- mock-testing-with-spring-unit/src/main/java/com/virendra/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.virendra.repository;
-
-import com.virendra.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
