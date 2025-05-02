b9181a0cb65a5651d5121dab7f3e09daf74a51e1
Virendra kashyap <66119893+virendra-kashyap@users.noreply.github.com>
2025-05-02 15:04:30 +0530

    Delete spring-boot-with-kafka-ui directory

--- spring-boot-with-kafka-ui/src/main/java/com/virendra/kafka/repository/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.virendra.kafka.repository;
-
-import com.virendra.kafka.entity.Record;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
