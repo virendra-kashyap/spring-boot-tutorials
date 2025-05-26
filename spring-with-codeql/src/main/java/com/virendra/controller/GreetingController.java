547948b4f27e3933bacd9429a577584028415c0e
virendra-kashyap <virendrakashyap244@gmail.com>
2025-05-26 11:41:28 +0530

    implement codeql.

---
+++ spring-with-codeql/src/main/java/com/virendra/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.virendra.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
